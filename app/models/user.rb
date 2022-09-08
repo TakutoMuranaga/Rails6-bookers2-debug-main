53935cea21a8d91f858a933859c07dce32a84754
TakutoMuranaga <takutomura2019@gmail.com>
2022-09-08 06:23:49 +0000

    検索機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,21 @@ class User < ApplicationRecord
     reverse_of_relationships.find_by(follower_id: user.id).present?
   end
   
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+  
   
   has_one_attached :profile_image
 
